7eb873363b73d56a199ef2e56a0fdac52c16dffb
Ammar505 <amaryasser@example.com>
2023-10-09 13:37:39 -0700

    6 print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print numbers using putchar
+ * Return: return 0
+ */
+int main(void)
+{
+	char num2;
+
+	for (num2 = 0; num2 < 10; num2++ )
+	{
+		putchar(num2);
+	}
+		putchar('\n');
+		return (0);
+}
