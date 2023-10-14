152c417364458c7af3e3ec1575d308cc9a09eaa6
Ammar505 <amaryasser@example.com>
2023-10-14 07:01:02 -0700

    print numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,30 @@
-/**
- * main - entry point
- * Description: print 100 digits with putchar
- * Return: return 0
- */
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  int i, j;
-  
-  for (i = 0; i < 9; i++)
-  {
-    for (j = i + 1; j < 10; j++)
-    {
-      putchar(i + '0');
-      putchar(j + '0');
-      if (i != 8 || j != 9) {
-        putchar(',');
-        putchar(' ');
-      }
-    }
-  }
-  
-  putchar('\n');
-  
-  return 0;
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
 
