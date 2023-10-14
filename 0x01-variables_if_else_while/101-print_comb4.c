36b3937284c81b71350181af4e8edf307d58e36c
Ammar505 <amaryasser@example.com>
2023-10-14 07:31:26 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,35 @@
-/**
- * main - entry point
- * Description: print numbers
- * Return: return 0
- */
-
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int digit1, digit2, digit3, digit4;
 
-	for (digit1 = 0; digit1 <= 7; digit1++)
+	for (digit1 = 0; digit1 <= 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
 		{
 			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 			{
-				putchar(digit1 + '0');
-				putchar(digit2 + '0');
-				putchar(digit3 + '0');
-
-			if (!(digit1 == 7 && digit2 == 8 && digit3 == 9))
+				for (digit4 = digit3 + 1; digit4 <= 9; digit4++)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(digit1 + '0');
+					putchar(digit2 + '0');
+					putchar(digit3 + '0');
+					putchar(digit4 + '0');
+					if (!(digit1 == 9 && digit2 == 8
+					&& digit3 == 7 && digit4 == 6))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
-
